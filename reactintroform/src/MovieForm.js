8f9555ad98d04ca4da6637b5e611475ae9166a0d
morelk19 <morelkhristian@gmail.com>
2023-02-27 14:56:17 -0500

    working on code

---
+++ reactintroform/src/MovieForm.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+
+class MovieForm extends Component {
+
+    // constructors are initialized when Form is created (instantiated)
+    // we set the initial state of our form and save it to state.
+    constructor(props){
+        super(props); //inheriting props from Component
+
+        // set the initial state of the Form
+        this.initalState = {
+              title: "",
+              actors: [""],
+              plot: "",
+              genre: "",
+              imdbRating: '',
+              director: "",
+              year: '',
+              dateAdded: new Date()
+        };
+
+        //set our initial state to state.
+        this.state = this.initalState;
+    }
+
+    handleChange = event => {
+        //get name, value out of target (element that changed)
+        const { name, value } = event.target;
+
+        //update our state 
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onFormSubmit = (event) => {
+        //standard code for onSubmits 
+        event.preventDefault();
+        
+        //set the current state of our form to the handle submit
+        this.props.addMovie(this.state);
+
+        //clear inputs by setting form to inital state
+        this.setState(this.initialState);
+        
+
+    }
+    render() {
+        //hook in data from state 
+        const { title, actors } = this.state;
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <label htmlFor="title">Title :</label>
+                <input
+                    type="text" 
+                    id="title" 
+                    name="title"
+                    value={title}
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="actors">Actors:</label>
+                <input 
+                    type="text" 
+                    id="actors" 
+                    name="actors"
+                    value={actors} 
+                    onChange={this.handleChange}
+                    /><br/>
+                <label htmlFor="plot">Plot:</label>
+                    <input 
+                    type="text" 
+                    id="plot" 
+                    name ="plot"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+                 <label htmlFor="plot">Plot:</label>
+                    <input 
+                    type="text" 
+                    id="plot" 
+                    name ="plot"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+            <label htmlFor="genre">Genre:</label>
+                    <input 
+                    type="genre" 
+                    id="genre" 
+                    name ="genre"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+              <label htmlFor="imdbRating">imdbRating:</label>
+                    <input 
+                    type="imdbRating" 
+                    id="imdbRating" 
+                    name ="imdbRating"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+              <label htmlFor="director">director:</label>
+                    <input 
+                    type="director" 
+                    id="director" 
+                    name ="director"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+              <label htmlFor="year">director:</label>
+                    <input 
+                    type="year" 
+                    id="year" 
+                    name ="year"
+                    value={""} 
+                    onChange={this.handleChange}
+                    /><br/>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default MovieForm
